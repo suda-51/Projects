5c7a06ebfabc68bfca0b53eee6409c6c276c596f
korabum <rizkiman@yahoo.co.id>
2015-08-25 22:58:19 +0700

    Added solution to Fast Exponentation

---
+++ Numbers/power.py
@@ -0,0 +1,21 @@
+"""
+Enter two numbers a and b (separated by newline) and the program will print a^b with O(log N) time complexity
+"""
+
+def pow(a, b):
+    """
+    Return a to the power of b
+    """
+    if(b == 0):
+        return 1
+    else:
+        temp = pow(a, b / 2)
+        if(b % 2 == 0):
+            return temp * temp
+        else:
+            return temp * temp * a
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    print pow(a, b)
